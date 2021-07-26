9ab4b3edbe636629c0710a1bdfee8dfd69d9ec00
Sad69 <77921406+sad696@users.noreply.github.com>
2021-07-26 13:09:19 +0545

    Update things_to_do_list.py

--- things_to_do_list.py
+++ things_to_do_list.py
@@ -5,11 +5,11 @@ while True:
 
 
     enter_work = input("enter the work you want to do : ")
-
-    print("THINGS TO DO LIST -------------")
     things_to_do_list.append(enter_work)
     
     if enter_work == "close":
+        print("THINGS TO DO LIST -------------")
+        things_to_do_list.pop()
         print(",".join(things_to_do_list))
         print("make sure to the things on things to do list !")
         break
